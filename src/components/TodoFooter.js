f65763567cf0ef2d1f07c88f5b628d547130ed44
utumenbayar-miu <84539629+utumenbayar-miu@users.noreply.github.com>
2022-04-12 09:58:12 -0500

    Delete src directory

--- src/components/TodoFooter.js
+++
@@ -1,7 +0,0 @@
-export default function TodoFooter(props) {
-  return (
-    <div style={{ backgroundColor: "#55ab6c" }}>
-      <h3>It is the footer component {props.somethingDisplayInFooter}</h3>
-    </div>
-  );
-}
